61081a2e1c9ac31b82874cb672bd1b2bf2db6bdc
Demo User <debian@beaglebone.localdomain>
2021-08-06 20:40:36 +1000

    First working server

--- Node+BoneScript/server.js
+++ Node+BoneScript/server.js
@@ -1,12 +1,36 @@
-var http = require("http");
-var b = require("bonescript");
-var fs = require("fs");
-//var index = fs.readFileSync('index.html');
+"use strict"
+
+const http = require("http");
+const b = require("bonescript");
+
+const powerOutPlus = "P8_8";
+const powerLedPlus = "P8_7";
+
+var powerLedValue = 0;
+var powerLedChangeTime = Date.now()
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    message = { Hello: "World" };
-    res.end(JSON.stringify(message));
+    let statusCode = 200;
+    if (req.url.endsWith('api/push')){
+      b.digitalWrite(powerOutPlus, b.HIGH);
+      setTimeout(function(){b.digitalWrite(powerOutPlus, b.LOW)}, 300);
+    }
+    else if(req.url.endsWith('api/read')){
+    }
+    else if(req.url.endsWith('api/kill')){
+      setTimeout(function(){process.exit(0)}, 1000);
+    }
+    else {
+      statusCode = 404;
+    }
+    res.writeHead(statusCode, { "Content-Type": "text/json" });
+    res.end(JSON.stringify({url: res.url, value: powerLedValue, timestamp: Date.now() - powerLedChangeTime}))
   })
   .listen(8081);
+
+b.pinMode(powerOutPlus, b.OUTPUT);
+b.digitalWrite(powerOutPlus, b.LOW);
+
+b.pinMode(powerLedPlus, b.INPUT);
+b.attachInterrupt(powerLedPlus, function(x){powerLedValue=x.value; powerLedChangeTime=Date.now()}, b.CHANGE);
