5b6e05e4d0dbe1a1f1b025b9795036c3b376961a
Christian Nielsen <nielsen@amrose.dk>
2021-08-06 22:07:22 +1000

    Update server.js

--- Node+BoneScript/server.js
+++ Node+BoneScript/server.js
@@ -6,27 +6,27 @@ const b = require("bonescript");
 const buttonOut = "P8_8";
 const ledIn = "P8_7";
 
-var powerLedValue = 0;
-var powerLedChangeTime = Date.now();
+var ledInValue = 0;
+var ledInChangeTime = Date.now();
 
 http
   .createServer(function (req, res) {
     let statusCode = 200;
     if (req.url.endsWith("api/push")) {
       b.digitalWrite(buttonOut, b.HIGH);
-      setTimeout(function () {
+      setTimeout(() => {
         b.digitalWrite(buttonOut, b.LOW);
       }, 300);
     } else if (req.url.endsWith("api/read")) {
     } else if (req.url.endsWith("api/kill")) {
-      setTimeout(function () {
+      setTimeout(() => {
         process.exit(0);
       }, 1000);
     } else {
       statusCode = 404;
     }
     res.writeHead(statusCode, { "Content-Type": "text/json" });
-    res.end(JSON.stringify({ url: res.url, value: powerLedValue, timestamp: Date.now() - powerLedChangeTime }));
+    res.end(JSON.stringify({ url: res.url, value: powerLedValue, timestamp: Date.now() - ledInChangeTime }));
   })
   .listen(8081);
 
@@ -34,10 +34,9 @@ b.pinMode(buttonOut, b.OUTPUT);
 b.digitalWrite(buttonOut, b.LOW);
 
 b.pinMode(ledIn, b.INPUT);
-
 b.attachInterrupt(
   ledIn,
-  function (x) {
+  (x) => {
     powerLedValue = x.value;
     powerLedChangeTime = Date.now();
   },
