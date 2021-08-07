4f10882ef405cb1e76aa1006c675d61e1e6c7d95
Christian Nielsen <nielsen@amrose.dk>
2021-08-07 13:40:05 +1000

    Added exception handling to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const http = require("http");
 const b = require("bonescript");
+const { setInterval } = require("timers/promises");
 
 const buttonOut = "P8_8";
 const ledIn = "P8_7";
@@ -22,31 +23,35 @@ b.attachInterrupt(
   b.CHANGE
 );
 
-var server = http.createServer((req, res) => {
-  let statusCode = 200;
-  if (req.url.endsWith("api/push")) {
-    b.digitalWrite(buttonOut, b.HIGH);
-    setTimeout(() => {
-      b.digitalWrite(buttonOut, b.LOW);
-    }, 200);
-  } else if (req.url.endsWith("api/read")) {
-  } else if (req.url.endsWith("api/kill")) {
-    setTimeout(() => {
-      process.exit(0);
-    }, 1000);
-  } else {
-    statusCode = 404;
+runServer = function () {
+  try {
+    http
+      .createServer((req, res) => {
+        let statusCode = 200;
+        if (req.url.endsWith("api/push")) {
+          b.digitalWrite(buttonOut, b.HIGH);
+          setTimeout(() => {
+            b.digitalWrite(buttonOut, b.LOW);
+          }, 200);
+        } else if (req.url.endsWith("api/read")) {
+        } else if (req.url.endsWith("api/kill")) {
+          setTimeout(() => {
+            process.exit(0);
+          }, 1000);
+        } else {
+          statusCode = 404;
+        }
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Request-Method", "*");
+        res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+        res.setHeader("Access-Control-Allow-Headers", "*");
+        res.writeHead(statusCode, { "Content-Type": "text/json" });
+        res.end(JSON.stringify({ url: res.url, value: ledInValue, timestamp: Date.now() - ledInChangeTime }));
+      })
+      .listen(8081);
+  } catch (ex) {
+    setTimeout(() => runServer, 3000);
   }
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Request-Method", "*");
-  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.writeHead(statusCode, { "Content-Type": "text/json" });
-  res.end(JSON.stringify({ url: res.url, value: ledInValue, timestamp: Date.now() - ledInChangeTime }));
-});
+};
 
-server.listen(8081);
-
-setInterval(() => {
-  if (!server.listening) server.listen(8081);
-}, 3000);
+runServer();
