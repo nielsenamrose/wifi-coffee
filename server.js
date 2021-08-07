116246de66af1b75d1a7695e268085fea3d367bf
Christian Nielsen <nielsen@amrose.dk>
2021-08-07 13:02:37 +1000

    Added watchdog to server

--- server.js
+++ server.js
@@ -9,31 +9,6 @@ const ledIn = "P8_7";
 var ledInValue = 0;
 var ledInChangeTime = Date.now();
 
-http
-  .createServer(function (req, res) {
-    let statusCode = 200;
-    if (req.url.endsWith("api/push")) {
-      b.digitalWrite(buttonOut, b.HIGH);
-      setTimeout(() => {
-        b.digitalWrite(buttonOut, b.LOW);
-      }, 200);
-    } else if (req.url.endsWith("api/read")) {
-    } else if (req.url.endsWith("api/kill")) {
-      setTimeout(() => {
-        process.exit(0);
-      }, 1000);
-    } else {
-      statusCode = 404;
-    }
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Request-Method", "*");
-    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.writeHead(statusCode, { "Content-Type": "text/json" });
-    res.end(JSON.stringify({ url: res.url, value: ledInValue, timestamp: Date.now() - ledInChangeTime }));
-  })
-  .listen(8081);
-
 b.pinMode(buttonOut, b.OUTPUT);
 b.digitalWrite(buttonOut, b.LOW);
 
@@ -46,3 +21,32 @@ b.attachInterrupt(
   },
   b.CHANGE
 );
+
+var server = http.createServer((req, res) => {
+  let statusCode = 200;
+  if (req.url.endsWith("api/push")) {
+    b.digitalWrite(buttonOut, b.HIGH);
+    setTimeout(() => {
+      b.digitalWrite(buttonOut, b.LOW);
+    }, 200);
+  } else if (req.url.endsWith("api/read")) {
+  } else if (req.url.endsWith("api/kill")) {
+    setTimeout(() => {
+      process.exit(0);
+    }, 1000);
+  } else {
+    statusCode = 404;
+  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.writeHead(statusCode, { "Content-Type": "text/json" });
+  res.end(JSON.stringify({ url: res.url, value: ledInValue, timestamp: Date.now() - ledInChangeTime }));
+});
+
+server.listen(8081);
+
+setInterval(() => {
+  if (!server.listening) server.listen(8081);
+}, 3000);
